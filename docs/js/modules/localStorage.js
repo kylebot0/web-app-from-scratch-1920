5da77c6eaebcb78b73f946bdfe50a578daa176a8
kylebot0 <kylebot07@gmail.com>
2020-05-15 12:18:22 +0200

    Add

--- docs/js/modules/localStorage.js
+++ docs/js/modules/localStorage.js
@@ -1,40 +1,49 @@
 import { getData } from "./data";
 
+const api = {
+    url: `https://api.spacexdata.com/v3/`
+};
+
+const requestOptions = {
+    method: "GET",
+    redirect: "follow"
+};
+
 async function checkLocalStorage(buttons) {
     const storage = createStorage();
     const items = storage.getItem();
     if (items == 0) {
-      console.log("1");
-      const data = await getData(buttons, api, requestOptions);
-      storage.setItem(data);
-      return data;
+        console.log("1");
+        const data = await getData(buttons, api, requestOptions);
+        storage.setItem(data);
+        return data;
     } else if (items.length > 1) {
-      return storage.getItem();
+        return storage.getItem();
     }
-  }
+}
 
 const createStorage = function() {
 
-    return{
-    getItem: function(){
-        if(window.localStorage.length == 0){
-            return window.localStorage.length
-        } else if(window.localStorage.items.length >= 1){
-            let parsed = JSON.parse(window.localStorage.getItem("items"));
-            return parsed;
-        }
-    },
-    setItem: function(data){
-        console.log(data)
-        return window.localStorage.setItem("items", JSON.stringify(data))
-    },
-    removeItem: function(){
+    return {
+        getItem: function() {
+            if (window.localStorage.length == 0) {
+                return window.localStorage.length
+            } else if (window.localStorage.items.length >= 1) {
+                let parsed = JSON.parse(window.localStorage.getItem("items"));
+                return parsed;
+            }
+        },
+        setItem: function(data) {
+            console.log(data)
+            return window.localStorage.setItem("items", JSON.stringify(data))
+        },
+        removeItem: function() {
 
-    },
-    clear: function(){
-        
+        },
+        clear: function() {
+
+        }
     }
-}
 }
 
 export { checkLocalStorage };
